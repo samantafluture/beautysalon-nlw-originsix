cd17b1eebdab8e31de979666f5d4ccc3fb8417e2
Samanta Fluture <samantafluture@gmail.com>
2021-11-01 20:23:14 -0200

    resize media queries

--- js/main.js
+++ js/main.js
@@ -33,7 +33,13 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-pagination'
     },
     mousewheel: true,
-    keyboard: true
+    keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
 });
 
 /* scroll reveal, mostra elementos com scroll */
